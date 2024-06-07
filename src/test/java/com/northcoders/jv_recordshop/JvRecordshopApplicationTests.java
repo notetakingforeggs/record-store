5febffa5c780eaaf80a8b1ba7b6bb2b6937b4cb4
notetakingforeggs <notetakingforeggs@gmail.com>
2024-06-07 10:03:37 +0100

    Initial Commit.

---
+++ src/test/java/com/northcoders/jv_recordshop/JvRecordshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.northcoders.jv_recordshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JvRecordshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
